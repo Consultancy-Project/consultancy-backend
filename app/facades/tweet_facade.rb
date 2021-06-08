60ff04ceb54cbdafb668c5523601451ea824538c
brueck1988 <brueck1988@gmail.com>
2021-06-07 18:39:54 -0600

    Refactor

--- app/facades/tweet_facade.rb
+++ app/facades/tweet_facade.rb
@@ -1,21 +1,22 @@
 class TweetFacade
   def self.recent_tweets
     tweet_data = TweetService.recent_tweets
-    
-    tweet_users = tweet_data[:includes][:users].map do |data|
-        data
-    end.first(3)
-    
-    tweet_data = tweet_data[:data].map do |data|
-        data
-    end.first(3)
-    
-    3.times do |n|
-      tweet_data[n][:username] = tweet_users[n][:username]
-    end
-    
-    tweet_data.map do |data|
+    new_data = combine_users_and_tweets(tweet_data)
+    new_data.map do |data|
         Tweet.new(data)
     end
   end
+  
+  def self.combine_users_and_tweets(tweet_data)
+    array = []
+    data = {}
+    3.times do |index|
+      data[:text] = tweet_data[:data][index][:text]
+      data[:id] = tweet_data[:data][index][:id]
+      data[:username] = tweet_data[:includes][:users][index][:username]
+      array << data
+    end
+    array
+  end
 end
+
