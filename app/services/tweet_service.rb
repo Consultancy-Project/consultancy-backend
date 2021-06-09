e87aa418c64f838f550c7f5e30dcda30e02271a5
brueck1988 <brueck1988@gmail.com>
2021-06-08 18:54:22 -0600

    Error handling Progress

--- app/services/tweet_service.rb
+++ app/services/tweet_service.rb
@@ -1,15 +1,19 @@
 class TweetService
+  def self.recent_tweets
+    response = conn.get("/2/tweets/search/recent?query=stocks&expansions=author_id&user.fields=name")
+    raise ExternalApiError if response.status != 200
+    parse_json(response)
+  end
+
+  private
+  
+  def self.parse_json(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+  
   def self.conn
     Faraday.new(url: 'https://api.twitter.com') do |faraday|
       faraday.headers['authorization'] = "Bearer #{ENV['twitter_token']}"
     end
-  end
-
-  def self.parse_json(response)
-    JSON.parse(response.body, symbolize_names: true)
-  end
-
-  def self.recent_tweets
-    parse_json(conn.get("/2/tweets/search/recent?query=stocks&expansions=author_id&user.fields=name"))
   end
 end
--- spec/requests/api/v1/tweets_request_spec.rb
+++ spec/requests/api/v1/tweets_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe 'Stock Tweets', :vcr do
-  it 'can get top 50 recent tweets' do
+RSpec.describe 'Stock Tweets' do
+  it 'can get top 50 recent tweets' do ####, :vcr
     get "/api/v1/tweets"
     tweets = JSON.parse(response.body, symbolize_names: true)
     expect(response).to be_successful
@@ -22,5 +22,41 @@ RSpec.describe 'Stock Tweets', :vcr do
       expect(tweet[:attributes]).to have_key(:username)
       expect(tweet[:attributes][:username]).to be_a(String)
     end
+  end
+  
+  it 'SAD PATH - Bad server response' do
+    # get "/api/v1/tweets"
+
+    json_response = {
+                      "title": "Unauthorized",
+                      "type": "about:blank",
+                      "status": 401,
+                      "detail": "Unauthorized"
+                    }
+
+    response_1 = stub_request(:get, "https://api.twitter.com/2/tweets/search/recent?expansions=author_id&query=stocks&user.fields=name").
+             with(
+               headers: {
+           	  'Accept'=>'*/*',
+           	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+           	  'Authorization'=>"Bearer #{ENV['twitter_token']}",
+           	  'User-Agent'=>'Faraday v1.4.2'
+               }).
+             to_return(status: 401, body: json_response.to_json, headers: {})
+    require "pry";binding.pry               
+    bad_response = TweetService.recent_tweets
+    # tweets = JSON.parse(response.body, symbolize_names: true)
+    expect(TweetService.recent_tweets).to raise_error(ExternalApiError)
+    expect(bad_response).to raise_error(ExternalApiError)
+
+    expect(tweets).to have_key(:errors)
+    expect(tweets[:errors]).to be_an(Array)
+
+    expect(tweet[:errors].first).to have_key(:status)
+    expect(tweet[:errors][0][:status]).to be_a(String)
+    expect(tweet[:errors].first).to have_key(:message)
+    expect(tweet[:errors][0][:message]).to be_a(String)
+    expect(tweet[:errors].first).to have_key(:code)
+    expect(tweet[:errors][0][:code]).to be_a(Integer)
   end
 end
